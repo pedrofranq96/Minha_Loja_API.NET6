1988a46efeb24dc72ed53c764a7220f6700b6c51
Pedro <peedrofranq96@gmail.com>
2022-12-30 16:57:07 -0300

    Finalizando os endpoints

---
+++ Endpoints/Categories/CategoryGetAll.cs
@@ -0,0 +1,24 @@
+﻿using ProdutosApp.Domain.Products;
+using ProdutosApp.Infra.Data;
+
+namespace ProdutosApp.Endpoints.Categories;
+
+public class CategoryGetAll
+{
+    //rota
+    public static string Template => "/categories";
+
+    //metodos de acesso que seram acessados pelo Get
+    public static string[] Methods => new string[] { HttpMethod.Get.ToString() };
+
+    //Chama a acao
+    public static Delegate Handle => Action;
+
+    public static IResult Action(ApplicationDbContext context)
+    {
+        var categories = context.Categories.ToList();
+        var response = categories.Select(c => new CategoryResponse { Id = c.Id, Name = c.Name, Active = c.Active });
+
+        return Results.Ok(response);
+    }
+}
---
+++ Endpoints/Categories/CategoryPut.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ProdutosApp.Domain.Products;
+using ProdutosApp.Infra.Data;
+
+namespace ProdutosApp.Endpoints.Categories;
+
+public class CategoryPut
+{
+    //rota
+    public static string Template => "/categories/{id}";
+
+    //metodos de acesso que seram acessados pelo Put
+    public static string[] Methods => new string[] { HttpMethod.Put.ToString() };
+
+    //Chama a acao
+    public static Delegate Handle => Action;
+
+    public static IResult Action([FromRoute]Guid id,CategoryRequest categoryRequest, ApplicationDbContext context)
+    {
+        var category = context.Categories.Where(c => c.Id == id).FirstOrDefault();
+        category.Name = categoryRequest.Name;
+        category.Active = categoryRequest.Active;
+        context.SaveChanges();
+
+        return Results.Ok();
+    }
+}
