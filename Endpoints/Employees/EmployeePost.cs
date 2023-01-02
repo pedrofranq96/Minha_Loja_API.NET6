693be9c1a99a61e18adc8352105d29ff74544b15
Pedro <peedrofranq96@gmail.com>
2023-01-02 17:51:49 -0300

    Criando o endpoint de Employee e configurando a senha de usuário

---
+++ Endpoints/Employees/EmployeePost.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using ProdutosApp.Domain.Products;
+using ProdutosApp.Infra.Data;
+
+namespace ProdutosApp.Endpoints.Employees;
+
+public class EmployeePost
+{
+    //rota
+    public static string Template => "/employees";
+
+    //metodos de acesso que seram acessados pelo POST
+    public static string[] Methods => new string[] { HttpMethod.Post.ToString() };
+
+    //Chama a acao
+    public static Delegate Handle => Action;
+
+    public static IResult Action(EmployeeRequest employeeRequest, UserManager<IdentityUser> userManager)
+    {
+        var user = new IdentityUser { UserName = employeeRequest.Email, Email = employeeRequest.Email };
+
+        var result = userManager.CreateAsync(user, employeeRequest.Password).Result;
+
+        if (!result.Succeeded)
+        {
+            return Results.BadRequest(result.Errors.First());
+        }
+              
+
+        return Results.Created($"/employees/{user.Id}", user.Id);
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,19 @@
 using Microsoft.AspNetCore.Identity;
 using ProdutosApp.Endpoints.Categories;
+using ProdutosApp.Endpoints.Employees;
 using ProdutosApp.Infra.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSqlServer<ApplicationDbContext>(builder.Configuration["ConnectionString:ProdutosApi"]);
-builder.Services.AddIdentity<IdentityUser, IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>(); //adicionando o identity como serviço do aspnet
+builder.Services.AddIdentity<IdentityUser, IdentityRole>(options => // configuracao de senha para o Identity user
+{
+    options.Password.RequireNonAlphanumeric = false;
+    options.Password.RequireDigit = false;
+    options.Password.RequireUppercase = false;
+    options.Password.RequiredLength = 3;
+    options.Password.RequireLowercase = false;
+
+}).AddEntityFrameworkStores<ApplicationDbContext>(); //adicionando o identity como serviço do aspnet
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
@@ -22,5 +31,6 @@ app.UseHttpsRedirection();
 app.MapMethods(CategoryGetAll.Template, CategoryGetAll.Methods, CategoryGetAll.Handle);
 app.MapMethods(CategoryPost.Template, CategoryPost.Methods, CategoryPost.Handle);
 app.MapMethods(CategoryPut.Template, CategoryPut.Methods, CategoryPut.Handle);
+app.MapMethods(EmployeePost.Template, EmployeePost.Methods, EmployeePost.Handle);
 app.Run();
 
