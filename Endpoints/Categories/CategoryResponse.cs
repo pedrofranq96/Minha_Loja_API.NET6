5ba6feeb1e1715abc43d9a71ab4446cf445a5eeb
Pedro <peedrofranq96@gmail.com>
2023-01-03 14:01:27 -0300

    Refatorando o Request e Response de Category

--- Endpoints/Categories/CategoryResponse.cs
+++ Endpoints/Categories/CategoryResponse.cs
@@ -1,8 +1,2 @@
 ﻿namespace ProdutosApp.Endpoints.Categories;
-
-public class CategoryResponse
-{
-    public Guid Id { get; set; }
-    public string Name { get;  set; }
-    public bool Active { get;  set; }
-}
+public record CategoryResponse(Guid Id, string Name, bool Active);
--- Endpoints/Employees/EmployeeRequest.cs
+++ Endpoints/Employees/EmployeeRequest.cs
@@ -1,3 +1,2 @@
 ﻿namespace ProdutosApp.Endpoints.Employees;
-
 public record EmployeeRequest(string Email, string Password, string Name, string EmployeeCode);
